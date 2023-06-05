09e8fab26b664bba76fa781cca0d45a22b140e92
Xavier <xavieruxchamp@gmail.com>
2023-06-05 15:28:04 -0700

    uploading my Calling Methods Assignment

---
+++ CallingMethodsAssignment/Computes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallingMethodsAssignment
+{
+    //Creating a class with three methods. The methods should do some math operation on the received parameter.
+    public class Computes
+    {
+
+    public static int Factorial(int number) //Each method will take one integer parameter in and return an integer.
+        {
+            if (number == 0)
+                return 1;
+            return number * Factorial(number - 1);
+        }
+
+    public static int Square(int number) //Each method will take one integer parameter in and return an integer.
+        {
+            int square = number * number;
+            return square;
+        }
+
+        public static int Cube(int number) //Each method will take one integer parameter in and return an integer.
+        {
+            int cube = number * number * number;
+            return cube;
+        }
+    }
+}
---
+++ CallingMethodsAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asking the user what number they want to do the math operations on.
+            Console.WriteLine("Please provide a number so that we could Square it, Cube it, and calculate its Factorial");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //Creating a class with three methods under a separate file
+
+            //Calling each method in turn, passing the user input to the method
+            int squaredNumber = Computes.Square(number);
+            int cubedNumber = Computes.Cube(number);
+            int factNumber = Computes.Factorial(number);
+
+            //Displaying each returned integer to the screen.
+            Console.WriteLine($"Squared: {squaredNumber}, Cubed: {cubedNumber}, Factorial: {factNumber}");
+            Console.ReadLine();
+
+        }
+    }
+}
