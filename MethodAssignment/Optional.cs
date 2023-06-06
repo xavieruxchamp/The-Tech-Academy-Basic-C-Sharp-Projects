d879424ecc62ebc5e9feae1d48d469e300f1de01
Xavier <xavieruxchamp@gmail.com>
2023-06-06 15:35:14 -0700

    uploading my Method Assignment

---
+++ MethodAssignment/Optional.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodAssignment
+{
+    class Optional
+    {
+        //Creating a class.
+
+        //Creating a method that takes two integers as parameters.
+        public static int Multiplication(int a, int b=1) //Making one of paramenters optional.
+        {
+            return a * b;  //Having the method do a math operation and return an integer result.
+        }
+    }
+}
---
+++ MethodAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating a class with a method that takes two integers as parameters, making one of them optional. Then, having the method do a math operation and return an integer result.
+
+            //Instantiating the class.
+            Console.WriteLine("Please enter the first number:"); //Asking the user to input two numbers, one at a time.
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the 2nd number: (Optional/Not required)"); //Letting them know that the 2nd number is optional.
+            string input = Console.ReadLine();
+
+            int num2;
+            bool hasNum2 = int.TryParse(input, out num2);
+
+            int result = hasNum2 ? Optional.Multiplication(num1, num2) : Optional.Multiplication(num1); //Calling the method in the class, passing in one or two numbers, as provided.
+            Console.WriteLine("Result: " + result);
+            Console.ReadLine();
+        }
+    }
+}
