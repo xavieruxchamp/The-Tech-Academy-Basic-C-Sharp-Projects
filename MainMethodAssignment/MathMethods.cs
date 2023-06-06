b01abbd22c4d92e87d025a260921dd8facc98aa1
Xavier <xavieruxchamp@gmail.com>
2023-06-06 13:55:02 -0700

    uploading my Main Method Assignment

---
+++ MainMethodAssignment/MathMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodAssignment
+{
+    class MathMethods
+    {
+        // Creating a class.
+        public static int mOperator(int a, int b) //Creating a method that will take in an integer, creating a math operation for this integer, then return the answer as an integer.
+        {
+            return a * b;
+         }
+
+        public static int mOperator(decimal c, decimal d) //Adding a second method to the class with the same name that will take in a decimal, to use it in a  different math operation, and to then return the answer as an integer.
+        {
+            double cDouble = (double)c;
+            double dDouble = (double)d;
+
+            return (int)Math.Pow(cDouble, dDouble);
+        }
+
+        public static int mOperator(string word) //Adding a third method to the class, with the same name, that will take in a string
+        {
+            int num = int.Parse(word); // Converting the string to an integer
+
+            int factorial = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial; // Returning the factorial result
+        }
+    }
+}
---
+++ MainMethodAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating a class, with 3 methods with unique signatures, in other workds, sharing the same name but with different parameters.
+            // Each of the methods perform different math opertations. 
+
+            //Instantiatng the class
+            //Calling the first method, passing in an integer.
+            int result1 = MathMethods.mOperator(5, 3);
+            Console.WriteLine("Result 1: " + result1);
+
+            //Instantiatng the class
+            //Calling the second method with decimal parameters
+            int result2 = MathMethods.mOperator(2.5m, 3.7m);
+            Console.WriteLine("Result 2: " + result2);
+
+            //Instantiatng the class
+            //Calling the third method with a string parameter
+            int result3 = MathMethods.mOperator("4");
+            Console.WriteLine("Result 3: " + result3);
+            Console.ReadLine();
+        }
+    }
+}
