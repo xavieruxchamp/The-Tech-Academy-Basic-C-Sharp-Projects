d7fadec227961608b03a8d9caac0e6974f347b94
Xavier <xavieruxchamp@gmail.com>
2023-06-07 15:16:01 -0700

    uploading my Method Class Assignment

---
+++ MethodClassAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating a class with a void method that takes two integers as parameters.
+            //Then, having the method do a math operation on the first integer and display the second integer to the screen.
+
+            //Instantiate the class.
+            TwoParam twoParam = new TwoParam();
+
+            twoParam.factDisp(2, 4); //Calling the method in the class, passing in two numbers.
+            twoParam.factDisp(a: 9, b: 3); //Calling the method in the class, specifying the parameters by name.
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MethodClassAssignment/TwoParam.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClassAssignment
+{
+    class TwoParam
+    {
+        //Create a class with a void method that takes two integers as parameters.
+
+        public void factDisp(int a, int b)  //Creating a method that takes two integers as parameters.
+        {
+            int factorial = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                factorial *= i; //Having the method do a math operation on the first integer
+            }
+            
+            Console.WriteLine("The Factorial of " + a  + " is: " + factorial + ". \nYou entered " + b + " as the second number.");; //Having the method display the second integer to the screen.
+        }
+    }
+}
