3938b0803bea2732913bb9e1d79d6a527abb050e
Xavier <xavieruxchamp@gmail.com>
2023-06-13 13:55:43 -0700

    Uploading my Abstract Class Assignment

---
+++ AbstractClassAssignment/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClassAssignment
+{
+    public class Employee : Person //Creating a derived class that inherits from an abstract class ( super class).
+    {
+        public override void SayName() //Overriding a method from an abstract class - implementing the SayName() method inside of the Employee class
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+
+    }
+}
---
+++ AbstractClassAssignment/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClassAssignment
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; } //Giving a property
+        public string LastName { get; set; } //Giving a property
+        public virtual void SayName() //Creating a public virtual void method
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+}
