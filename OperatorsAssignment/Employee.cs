84efdf8768add9c2f188ca2a9f03a8efe39b8f00
Xavier <44354258+xavieruxchamp@users.noreply.github.com>
2023-06-14 15:09:23 -0700

    uploading my Operators Assignment
    uploading my Operators Assignment

---
+++ OperatorsAssignment/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorsAssignment
+{
+    class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator ==(Employee employee1, Employee employee2) //Overloading the "==" operator.
+    
+        {
+            return employee1.ID == employee2.ID;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2) //Overloading the "!=" operator, since comparision operators must be overloaded in pairs.
+        {
+            return employee1.ID != employee2.ID;
+        }
+    }
+}
