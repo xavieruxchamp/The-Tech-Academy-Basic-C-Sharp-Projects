17431f215205c549fb0928ce62b04e3d9af5d015
Xavier <xavieruxchamp@gmail.com>
2023-06-15 10:33:40 -0700

    uploading my Parsing Enums Assignment

---
+++ ParametersAssignment/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParametersAssignment
+{
+    public class Employee
+    {
+        public static void dispItem<Things>(Things[] array) //Adding a property called “Things” with a generic list as its data type.
+        {
+            foreach (Things item in array) //Matching the generic type of the class
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+    
+}
---
+++ ParametersAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] employeeName = { "Joe", "Ann", "Pat" }; // Instantiating an Employee object with type “string”.
+            int[] employeeID = {1, 2, 3}; // Instantiating an Employee object with type “int”
+
+            Employee.dispItem(employeeName); //Assigning  a list of strings as the property value of Things.
+            Employee.dispItem(employeeID); //Assigning  a list of integers as the property value of Things.
+        }
+    }
+}
