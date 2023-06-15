3b0746d1c16496b0675fe7b01e68169348603d00
Xavier <xavieruxchamp@gmail.com>
2023-06-15 11:46:03 -0700

    uploading my Struct Assignment

---
+++ StructAssignment/Number.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructAssignment
+{
+    public struct Number
+    {
+        public decimal Amount; // Specifying the access modifier as public.
+    }
+}
---
+++ StructAssignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number newNumber; //Declaring a variable of type Number.
+
+            newNumber.Amount = 200.00m; //Assigning value to the variable
+        }
+    }
+}
