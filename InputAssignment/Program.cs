52e38e7a0a4c7b72221cc0cd019ab7d83c253863
Xavier <xavieruxchamp@gmail.com>
2023-06-18 13:19:56 -0700

    uploading my Input Assignment

---
+++ InputAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a numnber from 0 to 9:"); //Prompting user to enter a number
+            string text = Console.ReadLine(); //Reading input
+            File.WriteAllText(@"C:\Users\xavier\xavieruxchamp\The-Tech-Academy-Basic-C-Sharp-Projects\Logs\log.txt", text); //Wrting input into a text file
+
+            string fileContent = File.ReadAllText(@"C:\Users\xavier\xavieruxchamp\The-Tech-Academy-Basic-C-Sharp-Projects\Logs\log.txt"); //Reading text file
+            Console.WriteLine("You entered: " + fileContent); //Contents of text file
+            Console.ReadLine();
+        }
+    }
+}
