a34bde68b423ec35ba0acaf4f21c26be3854987f
Xavier <xavieruxchamp@gmail.com>
2023-06-26 11:20:38 -0700

    uploading my Entity Framework Assignment

---
+++ StudentsMVC/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentsMVC.Models;
+
+namespace StudentsMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact page - Academy of Learning.";
+
+            return View();
+        }
+        public ActionResult Instructor(int id)
+        {
+            ViewBag.Id = id;
+
+            Instructor dayTimeInstructor = new Instructor
+            {
+                Id = 1,
+                FirstName = "Erik",
+                LastName = "Gross"
+            };
+
+            return View(dayTimeInstructor);
+        }
+        public ActionResult Instructors()
+        {
+            List<Instructor> instructors = new List<Instructor>
+            {
+                new Instructor
+                {
+                    Id = 1,
+                    FirstName = "Rick",
+                    LastName = "Ramen"
+                },
+                new Instructor
+                {
+                    Id = 2,
+                    FirstName = "Brett",
+                    LastName = "Calendar"
+                },
+                new Instructor
+                {
+                    Id = 3,
+                    FirstName = "Adam",
+                    LastName = "Smithonian"
+                }
+            };
+            return View(instructors);
+        }
+    }
+}
